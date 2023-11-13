0719593e3514139e6baba6669cfc905d5261e61a
Naphtaliah224 <96439684+Naphtaliah224@users.noreply.github.com>
2023-11-13 14:58:21 -0500

    hello

---
+++ cal.py
@@ -0,0 +1,19 @@
+from calcul.calculator import*
+
+result5=calculate_exponential(2,7)
+result6=calculate_factorial(10)
+result7=calculate_square_root(36)
+
+#pati scientifique toujou
+result8=modulo(67,8)
+result9=racine_nieme(7,8)
+result10=carree(9)
+result12=valeur_absolue=(5)
+
+print("pou exponentielle:",result5)
+print("pou factorielle:",result6)
+print("pou racine_caarre:",result7)
+print(result8)
+print(result9)
+print(result10)
+print(result12)
